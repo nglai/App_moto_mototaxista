f0917d8b107cb9d0715c9bd468be1fe60f2f1820
nglai <nglaikoin@hotmail.com>
2021-08-06 10:53:08 -0300

    up logout

--- src/component/LogOut.jsx
+++ src/component/LogOut.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import firebase from '../../firebase';
+import { StyleSheet,Text, View, ActivityIndicator, FlatList, Button, Image } from 'react-native';
 
-export default function LogOut(){
+export default function LogOut({navigation}){
+    const [loading, setLoading] = useState(false);
+    const [state, setState] = useState([]);
+
+    
+    useEffect(
+        () => navigation.addListener('focus', () => {
+            pegaDados()
+        }), []
+    )
+
+    console.log(state);
+
+    //busca o conteúdo da coleção:
+    const  pegaDados = async () => {
+
+        //Referência do firebase firestore, acessando a coleção:
+        const motot = firebase.db.collection('mototaxista');
+
+        //constante de armazenamento esperando o retorno da função:
+        const resposta = await motot.get();
+
+        //constante que recebe os documentos alinhados no formato de array com as informações:
+        const dados = resposta.docs;
+
+        //Trazer um a um para receber e mostrar os dados organizados em objeto:
+        const listMotos = [];
+        dados.forEach(
+        doc => {
+            listMotos.push({
+                ...doc.data(),
+                key: doc.id
+            })
+        })    
+        setState(listMotos);
+        setLoading(false);
+      }
+
+    if(loading){
+        return <ActivityIndicator/>
+    }
+
     return(
         <View style={styles.container}>
-            <Text>Componente danibala logout</Text>
+            <FlatList
+                data={state}
+                renderItem={
+                    ({item})=>(
+                        <View style={styles.container}>
+                            <Text>Nome: {item.dados.nomeMotorista} </Text>
+                            <Text>Modelo Moto: {item.dados.modeloMoto} </Text>
+                            <Text>Placa: {item.dados.placa} </Text>
+                           
+                            <View style={styles.container2}>
+                            
+                        </View>
+                        </View>
+                )}/>
+                
         </View>
+        
     )
 }
 
