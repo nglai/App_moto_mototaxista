92a3199aa51af404278eb8bc83d5951db3e5a213
Vanessa Santos <83175312+vanmaira@users.noreply.github.com>
2021-08-09 14:26:12 -0300

    atualizafinal

--- src/component/Cliente.jsx
+++ src/component/Cliente.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import firebase from '../../firebase';
-import { StyleSheet,Text, View, ActivityIndicator, Button, Image } from 'react-native';
+import { StyleSheet,Text, View, ActivityIndicator } from 'react-native';
 
 export default function Corrida(){
     const [loading, setLoading] = useState(true);
@@ -26,23 +26,35 @@ export default function Corrida(){
 
     return(
         <View style={styles.container}>      
-            <View style={styles.container}>
-                <Text>Nome: {state.dados.nome}</Text>
-                <Text>Celular: {state.dados.celular}</Text>         
-            </View>
+            
+                <Text style={styles.h1}>Dados do Cliente:</Text>
+                <Text style={styles.texto}>Nome: {state.dados.nome}</Text>
+                <Text style={styles.texto}>Celular: {state.dados.celular}</Text>         
+            
         </View>
     )
 }
 const styles = StyleSheet.create({
     container:{
-        flex:1,
+        // flex:1,
         backgroundColor:'#FEA82F',
         alignItems:'center',
         justifyContent:'center',
+        borderWidth:2,
+        borderRadius:20,
+        borderColor:'#FCECDD',
+        margin:10,
+        padding:10
+    }, 
+     h1:{
+        fontSize:20,
+        margin:20,
+        fontWeight:'bold'
     },
-    logo:{
-        width:300,
-        height:300,
-        marginTop:'15%'
+    texto:{
+        fontSize:18,  
+    },
+    botoes:{
+        flexDirection:'row',
     }
   });
